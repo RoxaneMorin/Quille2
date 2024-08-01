b09abd24debc9b3611504bc28b3f8bf4dbbb3019
DouceNouni <roxy.perruche@hotmail.com>
2024-07-31 21:13:30 -0400

    Continued serializers.

--- Assets/Scripts/Utilities/UnityConverters/JsonConverter_SubjectiveNeedSO.cs
+++ Assets/Scripts/Utilities/UnityConverters/JsonConverter_SubjectiveNeedSO.cs
@@ -17,5 +17,32 @@ namespace Newtonsoft.Json.UnityConverters.Quille
             writer.WriteValue(value.name);
         }
     }
+
+    public class JsonConverter_SubjectiveNeedSOSubjectiveNeedDict : JsonConverter<SerializedDictionary<SubjectiveNeedSO, SubjectiveNeed>>
+    {
+        public override bool CanRead => true;
+
+        public override SerializedDictionary<SubjectiveNeedSO, SubjectiveNeed> ReadJson(JsonReader reader, Type objectType, SerializedDictionary<SubjectiveNeedSO, SubjectiveNeed> existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            SerializedDictionary<SubjectiveNeedSO, SubjectiveNeed> reconstructedDict = new SerializedDictionary<SubjectiveNeedSO, SubjectiveNeed>();
+
+            var theArray = Newtonsoft.Json.Linq.JArray.Load(reader);
+            foreach (var item in theArray)
+            {
+                SubjectiveNeed theNeed = JsonConvert.DeserializeObject<SubjectiveNeed>(item.ToString());
+                SubjectiveNeedSO theSO = theNeed.NeedSO;
+
+                reconstructedDict.Add(theSO, theNeed);
+            }
+
+            return reconstructedDict;
+        }
+        public override void WriteJson(JsonWriter writer, SerializedDictionary<SubjectiveNeedSO, SubjectiveNeed> value, JsonSerializer serializer)
+        {
+            // We assume the Key will match the Value's internal SubjectiveNeedSO, and therefore discard it.
+            string jsonArray = JsonConvert.SerializeObject(value.Values, Formatting.Indented);
+            writer.WriteRawValue(jsonArray);
+        }
+    }
 }
 
