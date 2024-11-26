35ddf419a3a5221bd2b0d27d8704d193688a3abf
DouceNouni <roxy.perruche@hotmail.com>
2024-11-25 20:19:02 -0500

    Updated pathConstants to add missing /

--- Assets/Scripts/PathConstants.cs
+++ Assets/Scripts/PathConstants.cs
@@ -9,21 +9,21 @@ public static class PathConstants
     // SCRIPTABLE OBJECTS
 
     // Checks and Modulators
-    public const string SO_PATH_CHECKS = "ScriptableObjects/ChecksAndMods/Checks";
-    public const string SO_PATH_MODULATORS = "ScriptableObjects/ChecksAndMods/Modulators";
+    public const string SO_PATH_CHECKS = "ScriptableObjects/ChecksAndMods/Checks/";
+    public const string SO_PATH_MODULATORS = "ScriptableObjects/ChecksAndMods/Modulators/";
 
     // Character
-    public const string SO_PATH_PERSONALITYAXES = "ScriptableObjects/Personality/Axes";
-    public const string SO_PATH_PERSONALITYTRAITS = "ScriptableObjects/Personality/Traits";
-    public const string SO_PATH_INTERESTS = "ScriptableObjects/Interests/Fields";
-    public const string SO_PATH_INTERESTDOMAINS = "ScriptableObjects/Interests/Domains";
+    public const string SO_PATH_PERSONALITYAXES = "ScriptableObjects/Personality/Axes/";
+    public const string SO_PATH_PERSONALITYTRAITS = "ScriptableObjects/Personality/Traits/";
+    public const string SO_PATH_INTERESTS = "ScriptableObjects/Interests/Fields/";
+    public const string SO_PATH_INTERESTDOMAINS = "ScriptableObjects/Interests/Domains/";
 
     // Needs
-    public const string SO_PATH_NEEDSBASIC = "ScriptableObjects/Needs/Basic";
-    public const string SO_PATH_NEEDSSUBJECTIVE = "ScriptableObjects/Needs/Subjective";
+    public const string SO_PATH_NEEDSBASIC = "ScriptableObjects/Needs/Basic/";
+    public const string SO_PATH_NEEDSSUBJECTIVE = "ScriptableObjects/Needs/Subjective/";
 
     // GENES
-    public const string SO_PATH_EYECOLOURS = "ScriptableObjects/Genes/EyeColours";
-    public const string SO_PATH_HAIRCOLOURS = "ScriptableObjects/Genes/HairColours";
-    public const string SO_PATH_SKINCOLOURS = "ScriptableObjects/Genes/Skintones";
+    public const string SO_PATH_EYECOLOURS = "ScriptableObjects/Genes/EyeColours/";
+    public const string SO_PATH_HAIRCOLOURS = "ScriptableObjects/Genes/HairColours/";
+    public const string SO_PATH_SKINCOLOURS = "ScriptableObjects/Genes/Skintones/";
 }
