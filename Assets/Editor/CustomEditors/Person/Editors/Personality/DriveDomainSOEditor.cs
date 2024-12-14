01836093eb96f927bf049a7769858c7a3d6731d5
DouceNouni <roxy.perruche@hotmail.com>
2024-12-14 14:05:13 -0500

    Custom editor for DriveDomainSOs. Created a few domains to test it out. Subfolders for custom Person editors.

---
+++ Assets/Editor/CustomEditors/Person/Editors/Personality/DriveDomainSOEditor.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Quille.DriveDomainSO))]
+public class DriveDomainSOEditor : Editor
+{
+    private SerializedProperty scriptProperty;
+
+    private SerializedProperty domainName;
+    private SerializedProperty domainIcon;
+    private SerializedProperty domainColour;
+    private SerializedProperty menuSortingIndex;
+
+    private SerializedProperty itemsInThisDomain;
+
+    private void OnEnable()
+    {
+        scriptProperty = serializedObject.FindProperty("m_Script");
+
+        domainName = serializedObject.FindProperty("domainName");
+        domainIcon = serializedObject.FindProperty("domainIcon");
+        domainColour = serializedObject.FindProperty("domainColour");
+        menuSortingIndex = serializedObject.FindProperty("menuSortingIndex");
+
+        itemsInThisDomain = serializedObject.FindProperty("itemsInThisDomain");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        // Draw script field.
+        using (new EditorGUI.DisabledScope(true))
+            EditorGUILayout.PropertyField(scriptProperty);
+        GUILayout.Space(EditorGUIUtility.singleLineHeight / 2);
+
+        // Draw generic properties.
+        EditorGUILayout.PropertyField(domainName);
+        EditorGUILayout.PropertyField(domainIcon);
+        EditorGUILayout.PropertyField(domainColour);
+        EditorGUILayout.PropertyField(menuSortingIndex);
+
+        GUILayout.Space(EditorGUIUtility.singleLineHeight / 2);
+
+        // Draw itemsInThisDomain.
+        EditorGUILayout.PropertyField(itemsInThisDomain);
+        GUILayout.Space(EditorGUIUtility.singleLineHeight / 2);
+
+        // TODO: implement this stuff.
+        //// Draw clean up button.
+        //if (GUILayout.Button("Clean Up & Fix Dangling References"))
+        //{
+        //    DrivesMenuUtilities.FixDriveReferences();
+        //}
+        //if (GUILayout.Button("Clean Up & Delete Dangling References"))
+        //{
+        //    DrivesMenuUtilities.DeleteDriveReferences();
+        //}
+
+        // Apply modified properties; modified linked objects as needed.
+        if (serializedObject.ApplyModifiedProperties())
+        {
+            Quille.DriveDomainSO targetDriveDomainSO = serializedObject.targetObject as Quille.DriveDomainSO;
+            UpdateReferencedDrives(targetDriveDomainSO);
+        }
+    }
+
+    private void UpdateReferencedDrives(Quille.DriveDomainSO domain)
+    {
+        foreach (Quille.DriveSO drive in domain.DrivesInThisDomain)
+        {
+            if (drive && !drive.InDriveDomains.Contains(domain))
+            {
+                drive.AddDomain(domain);
+                EditorUtility.SetDirty(drive);
+            }
+        }
+    }
+}
